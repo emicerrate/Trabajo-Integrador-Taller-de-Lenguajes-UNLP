a19aafaead0a7fba352287415a22fa5655d40882
EMILIO PABLO CERRATE <epcerrate@gmail.com>
2025-05-04 04:49:48 -0300

    Trabajo Integrador, Sección B: Implementación del punto 10.

--- src/funcionesT.py
+++ src/funcionesT.py
@@ -1,6 +1,78 @@
 import csv
 from collections import Counter
 
+def last_quarter(file_path):
+    """
+    Calcula el trimestre procesado en un archivo.
+    Args:
+        file_path (Path): Ruta al archivo del cual se leera.
+    Returns:
+        year (int): Año del trimestre más reciente procesado.
+        quarter (int): Número del trimestre más reciente procesado.
+    """
+    
+    with file_path.open("r", newline="") as file:
+        file_reader = csv.DictReader(file, delimiter=";")
+        year, quarter = max(((int(row["ANO4"]), int(row["TRIMESTRE"])) for row in file_reader), key=lambda x: (x[0], x[1]))
+    return year, quarter
+
+def compare_unfinished_high_school(path_individual):
+    """
+    Compara dos aglomerados según el porcentaje de personas mayores de edad con secundario incompleto.
+    Args:
+        path_individual (Path): Ruta al archivo usu_individual_final del cual se leera.
+    Returns:
+        None
+    """
+
+    # Guardo los aglomerados para asegurarme que el usuario ingresa aglomerados válidos
+    with path_individual.open("r", newline="") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        conglomerates = {row["AGLOMERADO"] for row in reader}
+
+    # Le pido al usuario los aglomerados a comparar
+    while True:
+        conglomerateA = input("Aglomerado A: ")
+        if conglomerateA not in conglomerates:
+            print("ERROR: El aglomerado que introdujo no es válido, por favor, intente de nuevo.")
+            continue
+        break
+
+    while True:
+        conglomerateB = input("Aglomerado B: ")
+        if conglomerateB not in conglomerates:
+            print("ERROR: El aglomerado que introdujo no es válido, por favor, intente de nuevo.")
+            continue
+        break
+
+    with path_individual.open("r", newline="") as file:
+        reader = csv.DictReader(file, delimiter=";")
+
+        # Creo un contador donde para cada año y trimestre me contara la cantidad de adultos por aglomerado, y
+        # hago otro donde calculo la cantidad de adultos que no terminaron el secundario para cada año y trimestre
+        # por aglomerado
+        adults = Counter()
+        unfinished_adults = Counter()
+
+        # Para cada persona verifico a que aglomerado pertenece y si es mayor de edad, y en caso de serlo,
+        # verifico si tiene el secundario incompleto
+        for person in reader:
+            if person["AGLOMERADO"] in (conglomerateA, conglomerateB) and int(person["CH06"]) >= 18:
+                adults.update({(person["AGLOMERADO"], person["ANO4"], person["TRIMESTRE"]): 1})
+                if person["NIVEL_ED_str"] == "SECUNDARIO INCOMPLETO":
+                    unfinished_adults.update({(person["AGLOMERADO"], person["ANO4"], person["TRIMESTRE"]): 1})
+        
+        # Guardo todos los años y trimestres para luego poder imprimirlos en orden
+        all_periods = {(year, quarter) for conglomerate, year, quarter in adults.keys()}
+        sorted_periods = sorted(all_periods, key=lambda x: (int(x[0]), int(x[1])))
+
+        # Imprimo para cada año y trimestre la comparación de porcentajes entre ambos aglomerados
+        print(f"{'Año':<6} {'Trimestre':<11} {'Aglomerado ' + conglomerateA:<20} {'Aglomerado ' + conglomerateB:<20}")
+        for year, quarter in sorted_periods:
+            percentageA = 100 * unfinished_adults.get((conglomerateA, year, quarter), 0) / adults[(conglomerateA, year, quarter)]
+            percentageB = 100 * unfinished_adults.get((conglomerateB, year, quarter), 0) / adults[(conglomerateB, year, quarter)]
+            print(f"{year:<6} {quarter:<11} {f'{percentageA:.2f}%':<20} {f'{percentageB:.2f}%':<20}")
+
 def retired_insufficient(path_individual, path_home):
     """
     Calcula el porcentaje de jubilados que viven en condiciones de habitabilidad insuficientes por aglomerado.
@@ -11,10 +83,8 @@ def retired_insufficient(path_individual, path_home):
         None
     """
 
-    # Calculo el ultimo trimestre y me guardo su numero y el año correspondiente
-    with path_home.open("r", newline="") as home:
-        home_reader = csv.DictReader(home, delimiter=";")
-        max_year, max_quarter = max(((int(house["ANO4"]), int(house["TRIMESTRE"])) for house in home_reader), key=lambda x: (x[0], x[1]))
+    # Calculo el ultimo trimestre y me guardo su número y el año correspondiente
+    max_year, max_quarter = last_quarter(path_home)
 
     with path_individual.open("r", newline="") as individual, path_home.open("r", newline="") as home:
         # Creo un contador donde para cada conglomerado me contara la cantidad de jubilados, y hago otro
@@ -25,7 +95,7 @@ def retired_insufficient(path_individual, path_home):
         individual_reader = csv.DictReader(individual, delimiter=";")
         home_reader = csv.DictReader(home, delimiter=";")
         
-        # Guardo las casas que corresponden al ultimo trimestre
+        # Guardo las casas que corresponden al último trimestre
         houses = {house["CODUSU"]: house for house in home_reader if int(house["ANO4"]) == max_year and int(house["TRIMESTRE"]) == max_quarter}
         
         # Para cada persona verifico si es o no jubilado, en caso de serlo, sus condiciones de habitabilidad
@@ -38,7 +108,7 @@ def retired_insufficient(path_individual, path_home):
                         retired_counter_insufficient.update({person["AGLOMERADO"]: 1})
         
         # Imprimo para cada aglomerado en orden ascendente su porcentaje
-        print("PORCENTAJE DE JUBILADOS VIVIENDO EN CONDICIÓN DE HABITABILIDAD INSUFICIENTE POR CONGLOMERADO:\n")
+        print("\nPORCENTAJE DE JUBILADOS VIVIENDO EN CONDICIÓN DE HABITABILIDAD INSUFICIENTE POR AGLOMERADO:")
         for conglomerate in sorted(retired_counter, key=int):
             percentage = (100 * retired_counter_insufficient.get(conglomerate, 0)) / retired_counter[conglomerate]
             print(f"Aglomerado {conglomerate}: {percentage:.2f}%")
