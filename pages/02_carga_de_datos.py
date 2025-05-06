ec603cb5b086deae51d5144b63744bb311859006
Alejandro Amor <alejandromanuelamor@gmail.com>
2025-05-06 13:35:24 -0300

    Se cambia de vuelta la estructura de streamlit. app.py se ejecuta desde el directorio principal para no generar conflictos con los path de las carpetas de los archivos y las importaciones.

---
+++ pages/02_carga_de_datos.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from src.funcionesA import range_dataset, init_file_reset
+
+min_year, max_year, min_quarter, max_quarter = range_dataset()
+
+st.header("Carga de datos")
+st.write(f'El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_quarter}.')
+if st.button("Actualizar dataset"):
+    init_file_reset()
+    st.success("Dataset actualizado correctamente.")
