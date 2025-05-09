5415c7b38546304332ab8aaf7f733585c79c5b5b
Franco Urricelqui <furricelqui@gmail.com>
2025-05-09 18:06:48 -0300

    Soluciona cálculo de fechas de los datasets

--- src/funcionesA.py
+++ src/funcionesA.py
@@ -5,14 +5,15 @@ from src.funcionesH import all_togetherH
 
 def range_dataset():
     home_init_file = DATA_PATH / "usu_hogar_inicial.csv"
-    years = set()
-    quarters = set()
+    quarters_for_year = {}
     with open(home_init_file, newline='', encoding="utf-8") as file:
         reader =csv.DictReader(file, delimiter=";")
         for row in reader:
-            years.add(int(row["ANO4"]))
-            quarters.add(int(row["TRIMESTRE"]))
-    return min(quarters), min(years), max(quarters), max(years)
+            quarters_for_year[int(row["ANO4"])] = set()
+            quarters_for_year[int(row["ANO4"])].add(int(row["TRIMESTRE"]))
+    min_year = min(quarters_for_year)
+    max_year = max(quarters_for_year)
+    return 3*(min(quarters_for_year[min_year]) - 1) + 1, min_year, 3 * max(quarters_for_year[max_year]), max_year
 
 def init_file_reset():
     home_init_file = DATA_PATH / "usu_hogar_inicial.csv"
