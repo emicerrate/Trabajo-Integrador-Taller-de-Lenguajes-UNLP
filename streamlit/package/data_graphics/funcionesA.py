14eaa796c27d1a8ea30ea57c72a370cd69d8d6ec
Franco Urricelqui <furricelqui@gmail.com>
2025-06-16 12:26:26 -0300

    agrega página 2

--- streamlit/package/data_graphics/funcionesA.py
+++ streamlit/package/data_graphics/funcionesA.py
@@ -47,19 +47,53 @@ def all_togetherI(path_salida):
                     for row in reader:
                         writer.writerow(row)
 
+def data_dates(route_file):
+    """Devuelve un diccionario con los trimestre que contiene el dataset para cada año"""
+    quarters_for_year = {}
+    # Se lee el archivo inicial
+    with open(route_file, newline='', encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            # Se genera un set para filtrar los trimestres y que se registren de manera única, el año es la key del diccionario
+            if int(row["ANO4"]) not in quarters_for_year.keys():
+                quarters_for_year[int(row["ANO4"])] = set()
+            quarters_for_year[int(row["ANO4"])].add(int(row["TRIMESTRE"]))
+    return quarters_for_year
+
+def check_dataset():
+    """Chequea que ambos 'usu_***_inicial.csv' tengan los mismos archivos según año y trimestre"""
+    home_init_file = DATA_PATH / "usu_hogar_inicial.csv"
+    indiv_init_file = DATA_PATH / "usu_individual_inicial.csv"
+    quarters_for_year_home = data_dates(home_init_file)
+    quarters_for_year_indiv = data_dates(indiv_init_file)
+    missing_files = []
+    # se guardan los trimestres que faltan en el archivo de individuos
+    for year in quarters_for_year_home:
+        if year not in quarters_for_year_indiv.keys():
+            for quarter in quarters_for_year_home[year]:
+                missing_files.append((year, quarter, "individuos"))
+        elif quarters_for_year_home[year] != quarters_for_year_indiv[year]:
+            for quarter in quarters_for_year_home[year]:
+                if quarter not in quarters_for_year_indiv[year]:
+                    missing_files.append((year, quarter, "individuos"))
+    # se guardan los trimestres que faltan en el archivo de hogares
+    for year in quarters_for_year_indiv:
+        if year not in quarters_for_year_home.keys():
+            for quarter in quarters_for_year_indiv[year]:
+                missing_files.append((year, quarter, "hogares"))
+        elif quarters_for_year_indiv[year] != quarters_for_year_home[year]:
+            for quarter in quarters_for_year_indiv[year]:
+                if quarter not in quarters_for_year_home[year]:
+                    missing_files.append((year, quarter, "hogares"))
+    return missing_files
+    
+
 def range_dataset():
     """
         Se calcula y devuelve el año maximo y minimo, y a partir del trimestre se agrega como fecha el mes de inicio de ese trimestre
     """
     home_init_file = DATA_PATH / "usu_hogar_inicial.csv"
-    quarters_for_year = {}
-    # Se lee el archivo inicial
-    with open(home_init_file, newline='', encoding="utf-8") as file:
-        reader = csv.DictReader(file, delimiter=";")
-        for row in reader:
-            # Se genera un set para filtrar los trimestres y que se registren de manera única, el año es la key del diccionario
-            quarters_for_year[int(row["ANO4"])] = set()
-            quarters_for_year[int(row["ANO4"])].add(int(row["TRIMESTRE"]))
+    quarters_for_year = data_dates(home_init_file)
     # Se calculan el min y el max
     min_year = min(quarters_for_year)
     max_year = max(quarters_for_year)
@@ -74,3 +108,4 @@ def init_file_reset():
     individual_init_file = DATA_PATH / "usu_individual_inicial.csv"
     all_togetherH(home_init_file)
     all_togetherI(individual_init_file)
+    
--- streamlit/pages/02_carga_de_datos.py
+++ streamlit/pages/02_carga_de_datos.py
@@ -1,11 +1,17 @@
 import streamlit as st
-from package.data_graphics.funcionesA import range_dataset, init_file_reset
+from package.data_graphics.funcionesA import range_dataset, init_file_reset, check_dataset
 
-init_file_reset()
 min_quarter, min_year, max_quarter, max_year = range_dataset()
 
 st.header("Carga de datos")
-st.write(f'El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
+
 if st.button("Actualizar dataset"):
     init_file_reset()
-    st.success("Dataset actualizado correctamente.")
+    st.write(f'El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
+    missing_files = check_dataset()
+    if missing_files == []:
+        st.success("Dataset actualizado correctamente.")
+    else:
+        st.error("Existe uno o más archivos faltantes en el dataset:")
+        for file in missing_files:
+            st.write(f'Archivo de {file[2]} correspondiente al trimestre {file[1]} del año {file[0]}')
