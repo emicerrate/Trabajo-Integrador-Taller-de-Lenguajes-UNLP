17df1610cc488ba23097dae0cddd210bcf63259b
Alejandro A <alejandromanuelamor@gmail.com>
2025-06-18 12:35:45 -0300

    Visualizacion pagina 2

--- streamlit/pages/02_carga_de_datos.py
+++ streamlit/pages/02_carga_de_datos.py
@@ -2,40 +2,44 @@ import streamlit as st
 from package.data_graphics.funcionesA import range_dataset, init_file_reset, check_dataset
 
 min_quarter, min_year, max_quarter, max_year = range_dataset()
-missing_files = check_dataset()
 
 st.title("🔄 Verificación y actualización de archivos EPH")
 
 st.write("""
 En esta sección podés verificar la **consistencia de los archivos cargados** para cada período de la Encuesta Permanente de Hogares (EPH).
-El sistema controla que:
-
-- Para cada archivo de **individuales** exista su correspondiente archivo de **hogares**.
-- Para cada archivo de **hogares** exista su correspondiente archivo de **individuales**.
 """)
+
+st.info(f'El Dataset actual contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
 
 st.write("""
-🔍 Si se detecta algún archivo faltante, se informará en pantalla el **año, trimestre y tipo de archivo** que no se encontró.
+## 🔍 Check Dataset para controlar:
 
-🖱️ **Actualizar datasets** para **resetear y procesar nuevamente** los archivos cargados
+- Que cada archivo de **individuales** exista su correspondiente archivo de **hogares**.
+- Que cada archivo de **hogares** exista su correspondiente archivo de **individuales**.
 """)
 
-st.info(f'El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
-if missing_files == []:
-    st.success("✅ Todos los archivos de hogares tienen su correspondiente de individuos.")
-else:
-    st.error("Existe uno o más archivos faltantes en el dataset:")
-    for file in missing_files:
-        st.write(f'Archivo de {file[2]} correspondiente al trimestre {file[1]} del año {file[0]}')
-
-
-if st.button("Actualizar dataset"):
-    init_file_reset()
-    st.write(f'El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
+if st.button("Check dataset"):
     missing_files = check_dataset()
     if missing_files == []:
-        st.success("Dataset actualizado correctamente. ✅ Todos los archivos de hogares tienen su correspondiente de individuos. ")
+        st.success("✅ Todos los archivos de hogares tienen su correspondiente de individuos. " \
+        "El sistema contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.")
     else:
         st.error("Existe uno o más archivos faltantes en el dataset:")
         for file in missing_files:
-            st.write(f'Archivo de {file[2]} correspondiente al trimestre {file[1]} del año {file[0]}')
+            st.error(f'Archivo de {file[2]} correspondiente al trimestre {file[1]} del año {file[0]}')
+
+st.write(""" 
+## 🖱️ Actualizar los datasets 
+""")
+
+if st.button("Actualizar dataset"):
+    init_file_reset()
+    missing_files = check_dataset()
+    if missing_files == []:
+        st.success("Dataset actualizado correctamente. ✅ Todos los archivos de hogares tienen su correspondiente de individuos.")
+        st.info(f'El Dataset actual contiene información desde el {min_quarter:02}/{min_year} hasta el {max_quarter:02}/{max_year}.')
+    else:
+        st.error("Existe uno o más archivos faltantes en el dataset:")
+        for file in missing_files:
+            st.error(f'Archivo de {file[2]} correspondiente al trimestre {file[1]} del año {file[0]}')
+
